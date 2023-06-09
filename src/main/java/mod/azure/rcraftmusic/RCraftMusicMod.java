5097dbe6a5f0e8f2da50662b19e0f19d2d16b531
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-06-09 13:02:13 -0400

    1.20 changes

--- src/main/java/mod/azure/rcraftmusic/RCraftMusicMod.java
+++ src/main/java/mod/azure/rcraftmusic/RCraftMusicMod.java
@@ -2,6 +2,11 @@ package mod.azure.rcraftmusic;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
@@ -11,64 +16,66 @@ public class RCraftMusicMod implements ModInitializer {
 	public static final String MODID = "rcraftmusic";
 	public static RCraftItems ITEMS;
 	public static RCraftSounds SOUNDS;
-
-	public static final CreativeModeTab RcraftItemGroup = FabricItemGroup.builder(new ResourceLocation(MODID, "rcraftmusic")).icon(() -> new ItemStack(RCraftItems.TONE_ITEM)).displayItems((context, entries) -> {
-		entries.accept(RCraftItems.TONE_ITEM);
-		entries.accept(RCraftItems.PRAIRIECLEARWEATHER_ITEM);
-		entries.accept(RCraftItems.PRAIRIECLOUDY_ITEM);
-		entries.accept(RCraftItems.PRAIRIEEVENING_ITEM);
-		entries.accept(RCraftItems.AVATARSPACE_ITEM);
-		entries.accept(RCraftItems.DESKTOP_ITEM);
-		entries.accept(RCraftItems.CASTLEDUNGEON_ITEM);
-		entries.accept(RCraftItems.CAVEDUNGEON_ITEM);
-		entries.accept(RCraftItems.DESERTFIELD_ITEM);
-		entries.accept(RCraftItems.EARTHFIELD_ITEM);
-		entries.accept(RCraftItems.FORESTFIELD_ITEM);
-		entries.accept(RCraftItems.MEATWALLDUNGEON_ITEM);
-		entries.accept(RCraftItems.PRAIRIERAIN_ITEM);
-		entries.accept(RCraftItems.SCORCHINGFIELD_ITEM);
-		entries.accept(RCraftItems.SNOWFIELDNIGHTFIELD_ITEM);
-		entries.accept(RCraftItems.STONEWALLDUNGEON_ITEM);
-		entries.accept(RCraftItems.WASTELANDFIELD_ITEM);
-		entries.accept(RCraftItems.WILDERNESSFIELD_ITEM);
-		entries.accept(RCraftItems.MENUOPEN_ITEM);
-		entries.accept(RCraftItems.GATEOPEN_ITEM);
-		entries.accept(RCraftItems.GATECLOSE_ITEM);
-		entries.accept(RCraftItems.LUMINA_ITEM);
-		entries.accept(RCraftItems.DESERTCLEAR_ITEM);
-		entries.accept(RCraftItems.DESERTCLOUDY_ITEM);
-		entries.accept(RCraftItems.DESERTNIGHTTIME_ITEM);
-		entries.accept(RCraftItems.DOLDONNA_ITEM);
-		entries.accept(RCraftItems.BREGEPONA_ITEM);
-		entries.accept(RCraftItems.TATARGANETSLUM_ITEM);
-		entries.accept(RCraftItems.FORESTOFPAIN_ITEM);
-		entries.accept(RCraftItems.ARCHEKLOENWATERFALL_ITEM);
-		entries.accept(RCraftItems.BALBOLARTMUSEUM_ITEM);
-		entries.accept(RCraftItems.BRIONAGWYDEONTHEDRAGONBEINRANGE_ITEM);
-		entries.accept(RCraftItems.CITYOFDAWNARSELRAY_ITEM);
-		entries.accept(RCraftItems.COITEBODHARBATTLEFIELD_ITEM);
-		entries.accept(RCraftItems.DEADWORLDOFINDIEGLUTLUGH_ITEM);
-		entries.accept(RCraftItems.GREATTEMPLEOFCAERLEONMEDB_ITEM);
-		entries.accept(RCraftItems.HULLEGRANZCATHEDRAL_ITEM);
-		entries.accept(RCraftItems.HYBRASIL_ITEM);
-		entries.accept(RCraftItems.INVERTEDCITYMEGINFI_ITEM);
-		entries.accept(RCraftItems.ISLANDOFKINGSHYBRASIL_ITEM);
-		entries.accept(RCraftItems.MORRIGUBARROWWALL_ITEM);
-		entries.accept(RCraftItems.SUGARMANSIONSIFBURG_ITEM);
-		entries.accept(RCraftItems.WAILINGCAPITALWALDUBERLISTERIN_ITEM);
-		entries.accept(RCraftItems.WARP_ITEM);
-		entries.accept(RCraftItems.LIAFAIL_ITEM);
-		entries.accept(RCraftItems.FORTOUPH_ITEM);
-		entries.accept(RCraftItems.MACANUR1_ITEM);
-		entries.accept(RCraftItems.CARMINAGADELICA_ITEM);
-		entries.accept(RCraftItems.DUNLORIAG_ITEM);
-		entries.accept(RCraftItems.EPITAPHOFTHETWILIGHT_ITEM);
-		entries.accept(RCraftItems.MACANUR2_ITEM);
-	}).build();
+	public static final ResourceKey<CreativeModeTab> RcraftItemGroup = ResourceKey.create(Registries.CREATIVE_MODE_TAB, new ResourceLocation(MODID, "rcraftmusic"));
 
 	@Override
 	public void onInitialize() {
 		ITEMS = new RCraftItems();
 		SOUNDS = new RCraftSounds();
+		Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, RcraftItemGroup, FabricItemGroup.builder().icon(() -> new ItemStack(RCraftItems.TONE_ITEM)) // icon
+				.title(Component.translatable("itemGroup.rcraftmusic.rcraftmusic")) // title
+				.displayItems((context, entries) -> {
+					entries.accept(RCraftItems.TONE_ITEM);
+					entries.accept(RCraftItems.PRAIRIECLEARWEATHER_ITEM);
+					entries.accept(RCraftItems.PRAIRIECLOUDY_ITEM);
+					entries.accept(RCraftItems.PRAIRIEEVENING_ITEM);
+					entries.accept(RCraftItems.AVATARSPACE_ITEM);
+					entries.accept(RCraftItems.DESKTOP_ITEM);
+					entries.accept(RCraftItems.CASTLEDUNGEON_ITEM);
+					entries.accept(RCraftItems.CAVEDUNGEON_ITEM);
+					entries.accept(RCraftItems.DESERTFIELD_ITEM);
+					entries.accept(RCraftItems.EARTHFIELD_ITEM);
+					entries.accept(RCraftItems.FORESTFIELD_ITEM);
+					entries.accept(RCraftItems.MEATWALLDUNGEON_ITEM);
+					entries.accept(RCraftItems.PRAIRIERAIN_ITEM);
+					entries.accept(RCraftItems.SCORCHINGFIELD_ITEM);
+					entries.accept(RCraftItems.SNOWFIELDNIGHTFIELD_ITEM);
+					entries.accept(RCraftItems.STONEWALLDUNGEON_ITEM);
+					entries.accept(RCraftItems.WASTELANDFIELD_ITEM);
+					entries.accept(RCraftItems.WILDERNESSFIELD_ITEM);
+					entries.accept(RCraftItems.MENUOPEN_ITEM);
+					entries.accept(RCraftItems.GATEOPEN_ITEM);
+					entries.accept(RCraftItems.GATECLOSE_ITEM);
+					entries.accept(RCraftItems.LUMINA_ITEM);
+					entries.accept(RCraftItems.DESERTCLEAR_ITEM);
+					entries.accept(RCraftItems.DESERTCLOUDY_ITEM);
+					entries.accept(RCraftItems.DESERTNIGHTTIME_ITEM);
+					entries.accept(RCraftItems.DOLDONNA_ITEM);
+					entries.accept(RCraftItems.BREGEPONA_ITEM);
+					entries.accept(RCraftItems.TATARGANETSLUM_ITEM);
+					entries.accept(RCraftItems.FORESTOFPAIN_ITEM);
+					entries.accept(RCraftItems.ARCHEKLOENWATERFALL_ITEM);
+					entries.accept(RCraftItems.BALBOLARTMUSEUM_ITEM);
+					entries.accept(RCraftItems.BRIONAGWYDEONTHEDRAGONBEINRANGE_ITEM);
+					entries.accept(RCraftItems.CITYOFDAWNARSELRAY_ITEM);
+					entries.accept(RCraftItems.COITEBODHARBATTLEFIELD_ITEM);
+					entries.accept(RCraftItems.DEADWORLDOFINDIEGLUTLUGH_ITEM);
+					entries.accept(RCraftItems.GREATTEMPLEOFCAERLEONMEDB_ITEM);
+					entries.accept(RCraftItems.HULLEGRANZCATHEDRAL_ITEM);
+					entries.accept(RCraftItems.HYBRASIL_ITEM);
+					entries.accept(RCraftItems.INVERTEDCITYMEGINFI_ITEM);
+					entries.accept(RCraftItems.ISLANDOFKINGSHYBRASIL_ITEM);
+					entries.accept(RCraftItems.MORRIGUBARROWWALL_ITEM);
+					entries.accept(RCraftItems.SUGARMANSIONSIFBURG_ITEM);
+					entries.accept(RCraftItems.WAILINGCAPITALWALDUBERLISTERIN_ITEM);
+					entries.accept(RCraftItems.WARP_ITEM);
+					entries.accept(RCraftItems.LIAFAIL_ITEM);
+					entries.accept(RCraftItems.FORTOUPH_ITEM);
+					entries.accept(RCraftItems.MACANUR1_ITEM);
+					entries.accept(RCraftItems.CARMINAGADELICA_ITEM);
+					entries.accept(RCraftItems.DUNLORIAG_ITEM);
+					entries.accept(RCraftItems.EPITAPHOFTHETWILIGHT_ITEM);
+					entries.accept(RCraftItems.MACANUR2_ITEM);
+				}).build()); // build() no longer registers by itself
 	}
 }
